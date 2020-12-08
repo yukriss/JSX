4881cf61ae6b13edde08fe0d8ee463e85e4f0064
Kriss. Samuel .Kilonzo <33784934+yukriss@users.noreply.github.com>
2020-12-08 11:05:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Here is your bus fare butter cup</h1>
+    <ul>
+      <li>Fare</li>
+      <li>rent</li>
+      <li>Utilities</li>
+      <li>Credit card</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
